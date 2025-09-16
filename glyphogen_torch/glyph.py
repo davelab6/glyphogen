Fix coordinate scaling

glyphogen_torch/glyph.py
@@ -17,6 +17,7 @@ from .command_defs import (
     NODE_GLYPH_COMMANDS,
     SVGCommand,
     NodeCommand,
+    MAX_COORDINATE,
 )
 
 
@@ -315,7 +316,9 @@ class NodeGlyph:
             command_vector = np.zeros(command_width, dtype=np.float32)
             command_vector[NodeCommand.encode_command(command.command)] = 1.0
             # Normalize and pad the coordinates
-            norm_coords = np.array(command.coordinates, dtype=np.float32) / 1000.0
+            norm_coords = (
+                np.array(command.coordinates, dtype=np.float32) / MAX_COORDINATE
+            )
             padded_coords = np.pad(
                 norm_coords, (0, max_coordinates - len(command.coordinates))
             )
@@ -365,7 +368,7 @@ class NodeGlyph:
             norm_coords = coord_tensor[i, :num_coords]
             if torch.is_tensor(norm_coords):
                 norm_coords = norm_coords.cpu().numpy()
-            denorm_coords = (norm_coords * 1000.0).round()
+            denorm_coords = (norm_coords * MAX_COORDINATE).round()
             coords = denorm_coords.astype(int).tolist()
 
             cur_contour.push_command(NodeCommand(command_str, coords))

glyphogen_torch/rasterizer.py
@@ -1,5 +1,5 @@
 import torch
-from .command_defs import NodeCommand
+from .command_defs import NodeCommand, MAX_COORDINATE
 from .hyperparameters import GEN_IMAGE_SIZE
 import pydiffvg
 import numpy as np
@@ -194,7 +194,8 @@ def rasterize_batch(cmds, coords, seed=42, img_size=None, requires_grad=True):
         point_start = 0
         for num_cp_end, point_end in zip(num_cp_splits, point_splits):
             num_cp = num_control_points[num_cp_start:num_cp_end]
-            path_points = points[point_start:point_end]
+            # Points in the model are normalized to -1 to 1, scale them back up
+            path_points = points[point_start:point_end] * MAX_COORDINATE
             path = pydiffvg.Path(
                 num_control_points=num_cp.to(torch.int32).cpu(),
                 points=path_points.cpu(),